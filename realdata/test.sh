8290178e7453eb183f400b6c400f7b81688d671c
Shinichi Morishita <moris@edu.k.u-tokyo.ac.jp>
2023-01-18 18:35:41 +0900

    Add files via upload

---
+++ realdata/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# executable modules
+uTR=../../uTR/uTR
+vTR=../vTR
+dir=.
+uTR_stat="uTR_stat.txt"
+
+genome="minisatellites_WDR7_ALS"
+unit=GTTATGTCCCTTACAGGATTTCACATATCCCTATTCTCAGGCAGGAATAGGGATATGTGAGCTATGATA
+TR_file=$dir/$genome".fasta"
+TR_uTR=$dir/$genome"_uTR.fasta"
+TR_vTR=$dir/$genome"_vTR.fasta"
+$uTR -f $TR_file -u $unit -stda -o $TR_uTR 1> $uTR_stat
+$vTR -f $TR_uTR > $TR_vTR
+
+genome="minisatellites_ABCA7_Alzheimer"
+unit=ACTCCCTCCCCGTGAGCCCCCCCACC
+TR_file=$dir/$genome".fasta"
+TR_uTR=$dir/$genome"_uTR.fasta"
+TR_vTR=$dir/$genome"_vTR.fasta"
+$uTR -f $TR_file -u $unit -stda -o $TR_uTR 1> $uTR_stat
+$vTR -f $TR_uTR > $TR_vTR
+
+genome="minisatellites_CACNA1C_Bipolor"
+unit=AATCACACGACCCTGACCTGACTAGTTTAC
+TR_file=$dir/$genome".fasta"
+TR_uTR=$dir/$genome"_uTR.fasta"
+TR_vTR=$dir/$genome"_vTR.fasta"
+$uTR -f $TR_file -u $unit -stda -o $TR_uTR 1> $uTR_stat
+$vTR -f $TR_uTR > $TR_vTR
+
+rm $uTR_stat
+exit 0
